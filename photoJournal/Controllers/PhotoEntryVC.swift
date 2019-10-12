d0442d83b3a165ae803db5d9d5bd8d9a3bd2502e
Radharani Ribas-Valongo <rad@Radharanis-MacBook-Pro.local>
2019-10-12 16:23:26 -0400

    adds placeholder text (for text view) and image (before photo is chosen)

--- photoJournal/Controllers/PhotoEntryVC.swift
+++ photoJournal/Controllers/PhotoEntryVC.swift
@@ -7,25 +7,149 @@
 //
 
 import UIKit
+import Photos
 
 class PhotoEntryVC: UIViewController {
     
     //MARK: -- Outlets
     
     @IBOutlet weak var photoImage: UIImageView!
-    
+    @IBOutlet weak var descriptionTextView: UITextView!
+    var placeholderLabel: UILabel!
     
     //MARK: -- Properties
     
-    //MARK: -- Functions
+    var photoLibraryAccess = false
+    
+    var image = UIImage() {
+        didSet {
+            photoImage.image = image
+        }
+    }
+    
+    //MARK: -- ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        checkPhotoLibraryAccess()
+        setUpTextViewPlaceholder()
+        setUpMiscellaneousViewDidLoadThings()
+        descriptionTextView.delegate = self
+    }
+    
+    
+    //MARK: -- IBActions
+    @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+        
     }
     
     @IBAction func cancelButton(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
     }
     
+    //MARK: -- OTHER Functions
+    
+    private func setUpTextViewPlaceholder() {
+        placeholderLabel = UILabel()
+        placeholderLabel.text = "Enter photo description..."
+        placeholderLabel.font = UIFont.italicSystemFont(ofSize: descriptionTextView.font?.pointSize ?? 10)
+        placeholderLabel.sizeToFit()
+        placeholderLabel.frame.origin = CGPoint(x: 5, y: (descriptionTextView.font?.pointSize)! / 2)
+        placeholderLabel.textColor = UIColor.lightGray
+        placeholderLabel.isHidden = !descriptionTextView.text.isEmpty
+        descriptionTextView.addSubview(placeholderLabel)
+    }
+    
+    private func setUpMiscellaneousViewDidLoadThings() {
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard)))
+        self.image = #imageLiteral(resourceName: "placeholder")
+    }
+    
+    @objc private func dismissKeyboard() {
+        descriptionTextView.resignFirstResponder()
+    }
+    
+    
+    //MARK: -- Photo Library Access
+    @IBAction func choosePhotoButtonPressed(_ sender: UIBarButtonItem) {
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        if self.photoLibraryAccess {
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController,animated: true)
+        } else {
+            let noAuthenticationAlertalertVC = UIAlertController(title: "ACCESS DENIED", message: "This app has not been authorized to access your photo library. :(", preferredStyle: .alert)
+            noAuthenticationAlertalertVC.addAction(UIAlertAction (title: "DENY", style: .destructive, handler: nil))
+            self.present(noAuthenticationAlertalertVC, animated: true, completion: nil)
+            
+            noAuthenticationAlertalertVC.addAction(UIAlertAction (title: "LET ME IN", style: .default, handler: { (action) in
+                self.photoLibraryAccess = true
+                self.present(imagePickerController, animated: true, completion: nil)
+            }))
+        }
+        
+    }
+    
+    
+    private func checkPhotoLibraryAccess() {
+        let status = PHPhotoLibrary.authorizationStatus()
+        
+        switch status {
+        case .authorized:
+            print(status)
+            self.photoLibraryAccess = true
+        case .notDetermined:
+            PHPhotoLibrary.requestAuthorization({status in
+                switch status {
+                case .authorized:
+                    self.photoLibraryAccess = true
+                    print(status)
+                case .denied:
+                    self.photoLibraryAccess = false
+                    print("denied")
+                case .notDetermined:
+                    print("not determined")
+                case .restricted:
+                    print("restricted")
+                }
+            })
+            
+        case .denied:
+            
+            let noAuthenticationAlert = UIAlertController(title: "Denied", message: "This app has not been authorized to access your photo library. Please change your settings", preferredStyle: .alert)
+            noAuthenticationAlert.addAction(UIAlertAction (title: "Ok", style: .default, handler: nil))
+            self.present(noAuthenticationAlert, animated: true, completion: nil)
+        case .restricted:
+            print("restricted")
+        }
+    }
+}
+
+//MARK: -- Image Picker Extension
+extension PhotoEntryVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+   
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let image = info[.originalImage] as? UIImage else {
+            print("Error grabbing image")
+            return
+        }
+        self.image = image
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
+
+
+
+//MARK: -- Text View extention
+extension PhotoEntryVC: UITextViewDelegate {
+
+    func textViewDidChange(_ textView: UITextView) {
+        placeholderLabel.isHidden = !textView.text.isEmpty
+    }
 }
